7d2547328b845fe69aced3360e0d4c390b63919a
ModeGirl <myroslava.partala@gmail.com>
2017-01-19 18:59:52 +0200

    new commit

---
+++ src/java1/class1.java
@@ -0,0 +1,17 @@
+package java1;
+
+
+
+/**
+ * My first class
+ */
+
+
+
+import java.util.Scanner;
+public class class1 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextLine());
+    }
+}
